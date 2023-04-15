52c02b57a48688c768d3419bcf6a2c9d5f359fd2
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2023-04-15 15:28:39 +0530

    inner html

--- DOM_MANIPULATION/script.js
+++ DOM_MANIPULATION/script.js
@@ -1,11 +1,6 @@
-// const body = document.body
-// const div  = document.createElement("div")
-//  div.innerText = "Hello World"
-// div.textContent = "Hello World 2 "
+const body = document.body
+const div  = document.createElement("div")
+ div.innerText = "Hello World"
+div.textContent = "Hello World 2 "
 
-// body.append(div)
-
-const div  = document.querySelector("div")
-
-console.log(div.textContent)
-console.log(div.innerText)
+body.append(div)
