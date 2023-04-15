fb1ae4aa57338b0b46fb7e1dfb414954868aa490
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2023-04-15 15:28:05 +0530

    diff between innerText and textContent

--- DOM_MANIPULATION/script.js
+++ DOM_MANIPULATION/script.js
@@ -1,6 +1,11 @@
-const body = document.body
-const div  = document.createElement("div")
-// div.innerText = "Hello World"
-div.textContent = "Hello World 2 "
+// const body = document.body
+// const div  = document.createElement("div")
+//  div.innerText = "Hello World"
+// div.textContent = "Hello World 2 "
 
-body.append(div)
+// body.append(div)
+
+const div  = document.querySelector("div")
+
+console.log(div.textContent)
+console.log(div.innerText)
